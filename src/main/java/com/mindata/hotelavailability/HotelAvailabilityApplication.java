a609b414bc4884f3f95c5135813aa20ea807de46
Emanuel Flores <emanuelzero@gmail.com>
2026-04-21 23:52:33 -0300

    Estructura inicial

---
+++ src/main/java/com/mindata/hotelavailability/HotelAvailabilityApplication.java
@@ -0,0 +1,12 @@
+package com.mindata.hotelavailability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAvailabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelAvailabilityApplication.class, args);
+    }
+}
